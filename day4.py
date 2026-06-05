3d3e3a64b5c4ef12182b16a686ce9ff43485aa64
bhushan-codes01 <bhushanwanere@gmail.com>
2026-06-06 01:01:38 +0530

    Added day4.py and updated day2.py

---
+++ day4.py
@@ -0,0 +1,82 @@
+# dictionary i python
+# info = {
+# "name ": "bhushan ",
+# "subjects ": ["os" , "pythom", "java"],
+# "age": 20,
+# "is_adult": True,
+# "marks": 94.1,
+# }
+
+# print(info["name "])
+# print(info["subjects "])
+# info["height"] = 78
+# print(info)
+
+# types of dictonary 
+# student = {
+#     "name ": "bhushan",
+#     "subjects" : {
+#         "phy ": 78,
+#         "maths": 45,
+#         "english": 67,
+#     }
+# }
+# print(student["subjects"]["maths"])
+
+#dictionary methods 
+# student = {
+#     "name": "bhushan",
+#     "subjects" : {
+#         "phy ": 78,
+#         "maths": 45,
+#         "english": 67,
+#     }
+# }
+# print(list(student.items()))
+# print(student.get("name"))
+
+#set  In  python
+
+# nums = {1,2,3,4,4,5,6,1,2,3,7,5,6,7}
+# print(type(nums))
+# print(nums)
+# print(len(nums))
+# num = {1,2,3,4,5,6,7,8,9}
+# print(num)
+# nums.add("bhushaN")
+# print(nums)
+# nums.pop()
+# # print(nums)
+# print(nums.union(num))
+# print(nums.intersection(num))
+
+# pratice questions
+
+# dic = {
+#     "cat": "a samall animal",
+#     "table": ["a piece of furniture" , " list of figures and figures"]
+# }
+# print(dic)
+
+# pratice4 quesiotrns
+
+# set = { "python" , "java" , " c++" , "python" , "javascript" , "java" , "pythom" , "java" , "c++" , "c" }
+# print(len(set))
+
+# practrice question 
+# marks = {}
+
+# sub1 = input("Enter subject 1 name: ")
+# marks.update({sub1: int(input(f"Enter marks of {sub1}: "))})
+
+# sub2 = input("Enter subject 2 name: ")
+# marks.update({sub2: int(input(f"Enter marks of {sub2}: "))})
+
+# sub3 = input("Enter subject 3 name: ")
+# marks.update({sub3: int(input(f"Enter marks of {sub3}: "))})
+
+# print(marks)
+
+# pratice questionbs
+values = {9,9.0}
+print(values)
